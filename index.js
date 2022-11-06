81d3d36ea53e5cf504c349d9257fd69d4660af9e
maevacrh <maeva.carinha@icloud.com>
2022-11-06 14:52:16 +0100

    challenge

---
+++ index.js
@@ -0,0 +1,17 @@
+function counter(){
+   firstResult=0
+for (i=1;i<2;i++){
+   console.log(firstResult,i)
+}
+for(j=1;j<2;j++){
+    console.log(firstResult,j,i)
+    for(k=3;k<4;k++)
+console.log(firstResult,j,i,k)
+for(l=3;l<4;l++){
+console.log(firstResult,j,i,l,k)}
+}
+}
+
+
+
+
