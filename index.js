d62d201f572db0ecfa102eff23cdb8a5dbb6ab9c
maevacrh <maeva.carinha@icloud.com>
2022-11-06 15:02:10 +0100

    challenge partially completed

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 function counter(){
    firstResult=0
 for (i=1;i<2;i++){
-   console.log(firstResult,i)
+   //console.log(firstResult,i)
+   let elementOne = firstResult + '-' + i;
+   document.getElementById("countertext").innerHTML= elementOne;
 }
 for(j=1;j<2;j++){
-    console.log(firstResult,j,i)
-    for(k=3;k<4;k++)
-console.log(firstResult,j,i,k)
-for(l=3;l<4;l++){
-console.log(firstResult,j,i,l,k)}
+    //console.log(firstResult,j,i)
+    let elementTwo = firstResult + '-'+j+'-'+i;
+    document.getElementById("countertext2").innerHTML= elementTwo;
+    for(k=3;k<4;k++){
+//console.log(firstResult,j,i,k)
+let elementThree = elementTwo +'-'+ k;
+document.getElementById("countertext3").innerHTML= elementThree;
+for(l=4;l<5;l++){
+//console.log(firstResult,j,i,l,k)}
+let elementFour = elementTwo +'-'+ k+'-'+l;
+document.getElementById("countertext4").innerHTML= elementFour;
+
 }
 }
 
+}
 
-
-
+}
